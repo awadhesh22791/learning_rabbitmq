b9819f0fdae70ef8c4bbdcf590398487344fdf1f
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-03-14 21:29:15 +0530

    send not to more than one message to worker.

--- 002/Recieve.py
+++ 002/Recieve.py
@@ -5,13 +5,14 @@ connection=pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel=connection.channel()
 if channel.is_open:
     print("Channel opened.")
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='hello',durable=True)
     def callback(ch,method,properties,body):
         print("[x] Recie4ved %r"%body)
         time.sleep(body.count(b'.'))
         print('[x] Done')
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
+    channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue="hello",
                             on_message_callback=callback)
     print('[*] Waiting for messages. To exist CTRL+C')
--- 002/Send.py
+++ 002/Send.py
@@ -5,11 +5,14 @@ connection=pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel=connection.channel()
 if channel.is_open:
     print("Channel opened.")
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='hello',durable=True)
     message=''.join(sys.argv[1:]) or 'Hello World'
     channel.basic_publish(exchange='',
                             routing_key="hello",
-                            body=message)
+                            body=message,
+                            properties=pika.BasicProperties(
+                                delivery_mode=2,#Make message persistent
+                            ))
     print(" [x] Sent %r"%message)
     connection.close()
 else:
