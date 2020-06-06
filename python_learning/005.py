60928fcc58b9399b66777793706ba31fe12515d3
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-06 15:30:55 +0530

    Completed 5th program

--- python_learning/005.py
+++ python_learning/005.py
@@ -1,1 +1,17 @@
 # 005. Write a program to display the fibonnaic series from 0 to 100
+def getFibonacci(limit):
+    series=[0,1,1]
+    next=series[series.__len__()-2]+series[series.__len__()-1]
+    while next < limit:
+        series.append(next)
+        next=series[series.__len__()-2]+series[series.__len__()-1]
+    return series
+
+print("Program to print fibonacci series upto limit.")
+limit=""
+while not limit.isnumeric():
+    limit=input("Enter upper limit of fibonacci series: ")
+fibonacciSeries=[]
+print("Generating series...")
+fibonacciSeries=getFibonacci(int(limit))
+print(fibonacciSeries.__str__())
