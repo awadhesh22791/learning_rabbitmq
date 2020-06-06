1b93e8c5ab1df0ca9db294f27ab311b2a991af1d
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-06 20:02:32 +0530

    Completed 6th problem.

--- python_learning/006.py
+++ python_learning/006.py
@@ -4,3 +4,15 @@
 #      * * * 
 #     * * * * 
 #    * * * * *
+number=""
+while not number.isnumeric():
+    number=input("Enter limit to print pattern: ")
+number=int(number)
+for i in range(number):
+    for j in range(number-i):
+        print(" ",end="")
+    k=0
+    for k in range(i+1):
+        print("* ",end="")
+        k+=1
+    print("")
