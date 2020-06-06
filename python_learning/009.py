05df0764c7fb7b8d79c08eebad4bc65b9cb50502
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-06 15:41:04 +0530

    Completed 9th program.

--- python_learning/009.py
+++ python_learning/009.py
@@ -1,2 +1,16 @@
 # 009. Write a program in which the function should take an input as a parameter in which the function should 
 # calculate the factorial of a given number.
+def factorial(number):
+    factorial=1
+    i=1
+    while i <=number:
+        factorial*=i
+        i+=1
+    return factorial
+
+number=""
+while not number.isnumeric():
+    number=input("Enter number to calculate factorial: ")
+print("Calculating factorial of "+number)
+factorialOfNumber=factorial(int(number))
+print("Factorial of "+number.__str__()+" is "+factorialOfNumber.__str__())
