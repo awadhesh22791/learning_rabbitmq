81b23833d0b35267a29e032cc7424b82040bc1b0
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-06 15:52:05 +0530

    Completed 11th program.

--- python_learning/011.py
+++ python_learning/011.py
@@ -1,1 +1,17 @@
 # 011. Write a program to find whether a given number is Armstrong number or not.
+def isArmstrong(number):
+    armstrong=True
+    sum=0
+    for character in str(number):
+        sum+=int(character) ** str(number).__len__()
+    armstrong=(sum==number)
+    return armstrong
+
+number=""
+while not number.isnumeric():
+    number=input("Enter number to check armstrong: ")
+print("Processing...")
+if isArmstrong(int(number)):
+    print(number+" is armstrong.")
+else:
+    print(number+" is not armstrong.")
