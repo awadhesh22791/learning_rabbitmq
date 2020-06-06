c86cbb7dfc48695d15886e31ac744675c8e0865e
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-06 14:22:23 +0530

    Completed second program.

--- python_learning/002.py
+++ python_learning/002.py
@@ -1,1 +1,13 @@
 # 002. Write a program in which lowercase letters should come first for a given string.
+print("Program in which lowercase letters should come first for a given string.")
+inputString=input("Enter String: ")
+while inputString.__len__()==0:
+    inputString=input("Enter String or ctrl+c to exit program: ")
+print('Processing "'+inputString+'"')
+processedString=''
+for character in inputString:
+    if character.islower():
+        processedString=character+processedString
+    else:
+        processedString+=character
+print('Processed string is "'+processedString+'"')
