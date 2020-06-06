f82c49ba0de4b86e57ed97cb71814e0d7977111c
Awadhesh Kumar <awadhesh22791@gmail.com>
2020-06-06 15:19:21 +0530

    Completed 4th program.

--- python_learning/004.py
+++ python_learning/004.py
@@ -1,1 +1,12 @@
 # 004. Given a string you should check a whether it is Palindrome or Not
+inputString=""
+print("Program to check string is palindrop or not.")
+while inputString.__len__()==0:
+    inputString=input("Enter string to check: ")
+reverseString=""
+for character in inputString:
+    reverseString=character+reverseString
+if inputString.__eq__(reverseString):
+    print('Your string "'+inputString+'" is palindrome.')
+else:
+    print('Your string "'+inputString+'" is not palindrome.')
